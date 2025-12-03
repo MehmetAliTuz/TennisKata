787e26a67f7422b74b63bed6e23bca6b9cf7e7e5
MehmetAliTuz <mehmetali@rentech.be>
2025-12-03 19:07:26 +0100

    Initial project setup: created solution structure with Console project and Test project.

---
+++ TennisKata.Tests/Game/TennisGameTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisKata.Tests.Game
+{
+    internal class TennisGameTests
+    {
+    }
+}
